88911ded9f1d502f6b438bd0e5f50e7b77a2d90f
SarumiofLagos <sarumioluwaseunideas@gmail.com>
2022-10-18 17:09:08 -0700

    task8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * Main - The art is useful
+ *
+ * Return - Always 1 (Successful)
+ */
+
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
