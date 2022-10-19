3ff3aed24c245897b7277b466c7a82abb20543ba
SarumiofLagos <sarumioluwaseunideas@gmail.com>
2022-10-19 16:23:47 -0700

    task3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print small and capital letters
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	char small-a = 'a';
+	char small-z = 'z';
+	char Big-A = 'A';
+	char Big-Z = 'Z';
+
+	for (; small-a <= small-z; small-a++)
+	{
+		putchar(small-a);
+	}
+	for (; Big-A <= Big-Z; Big-A++)
+	{
+		putchar(Big-A);
+	}
+	putchar('\n');
+	return (0)
+}
