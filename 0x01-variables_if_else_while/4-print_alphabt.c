08221d4be85d85f220134049607e0ac00d9244b0
SarumiofLagos <sarumioluwaseunideas@gmail.com>
2022-10-19 16:53:43 -0700

    task4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print letters but q and e
+ *
+ * return: 0 (success)
+ */
+
+int main(void)
+{
+	int noq_e;
+
+	for (; noq_e = 'a'; noq_e <= 'z'; noq_e++)
+	{
+		if (noq_e != 'q' && noq_e != 'e')
+		{
+			putchar(noq_e);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
