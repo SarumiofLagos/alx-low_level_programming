9a23165e1081d33ad2439aa1e2a7e62413f3b112
SarumiofLagos <sarumioluwaseunideas@gmail.com>
2022-10-20 13:01:28 -0700

    task6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print single digit base 10
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+
+	int a = 0;
+
+	while (a < 10)
+
+	{
+		putchar("%d", a);
+		a++;
+	}
+	putchar("\n");
+	return (0)
+}
