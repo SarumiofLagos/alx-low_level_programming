7b7976028970953a9e4c2c1935238aa84c62c8f9
SarumiofLagos <sarumioluwaseunideas@gmail.com>
2022-10-20 15:33:41 -0700

    task8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * main - print numbers of base 16
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+	for (n = 'a'; n <= 'f'; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
