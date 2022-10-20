741af68aabc5c58760117e4ef503c82a9dd46112
SarumiofLagos <sarumioluwaseunideas@gmail.com>
2022-10-20 13:38:25 -0700

    task7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print reverse small letters
+ *
+ * Return - 0 (end)
+ */
+
+int main(void)
+{
+
+	int a;
+
+	for (; a = 'z'; a >= 'a'; a--)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
