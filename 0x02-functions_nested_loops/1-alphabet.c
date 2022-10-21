ae9c4754ee55f557daba04b3e8714add7afcd500
SarumiofLagos <sarumioluwaseunideas@gmail.com>
2022-10-21 04:04:39 -0700

    task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - print lower case
+ *
+ * Return: 0 (end)
+ */
+
+int main(void)
+{
+
+	int a;
+
+	for ("a = 'a'; a<= 'z'\n"; a++)
+	{
+		putchar(a);
+	}
+
+	return (0);
+}
