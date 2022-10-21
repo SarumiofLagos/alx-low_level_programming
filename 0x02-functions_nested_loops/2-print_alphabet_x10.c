c4304a5343ef8e0b81376d8c8774a58abb915986
SarumiofLagos <sarumioluwaseunideas@gmail.com>
2022-10-21 06:13:02 -0700

    task2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print 10 times letters
+ *
+ * Return: 0 (end)
+ */
+
+void print_alphabet_x10(void)
+{
+
+	int n, x;
+
+	for (n = 0; n <= 9; n++)
+	{
+		for (x = 'a'; x <= 'z'; x++)
+		{
+			_putchar(b);
+		}
+		_putchar('\n');
+	}
+	return (0);
+
+}
