8980a4210e886d84163ffaf9dcde9b78bfae01f4
SarumiofLagos <sarumioluwaseunideas@gmail.com>
2022-10-25 11:17:36 -0700

    task2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - return length of string
+ * @n: input
+ * Return: 0 (end)
+ */
+
+int _strlen(char *s)
+
+{
+
+	_str len =0;
+
+	while (*str++)
+		len++;
+
+	return (0);
+
+}
