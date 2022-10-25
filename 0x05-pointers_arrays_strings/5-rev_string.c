c030d4358e57e2db7ad7fe8b99629f2fb43acfb4
SarumiofLagos <sarumioluwaseunideas@gmail.com>
2022-10-25 13:52:36 -0700

    task5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -3,23 +3,20 @@
 
 /**
  * rev_string - string reversed
- *
  * @s: reverse the string
  */
-
 void rev_string(char *s)
 {
+int i = 0, len = 0;
+char tmp;
 
-	int i = 0, len = 0;
-	char tmp;
+while (s[i++])
+len++;
 
-	while (s[i++])
-		len++;
-
-	for (i = len - 1; i >= len / 2; i--)
-	{
-		tmp = s[i];
-		s[i] = s[len - i - 1];
-		s[len -i - 1] = tmp;
-	}
+for (i = len - 1; i >= len / 2; i--)
+{
+tmp = s[i];
+s[i] = s[len - i - 1];
+s[len -i - 1] = tmp;
+}
 }
