fb18cd1c7943ccad6a7cd95377b03b8199ee64d3
SarumiofLagos <sarumioluwaseunideas@gmail.com>
2022-10-25 12:47:21 -0700

    task4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/* print_rev - print string in reverse
+ *
+ * @s: string to be reversed
+ */
+
+void print_rev(char *s)
+
+{
+
+	int len = 0, i = 0;
+
+	while (s[i++])
+		len++;
+
+	for (i = len - 1; i >= 0; i--)
+		_putchar(s[i]);
+
+	_putchar('\n');
+}
