f93352cb5c6e78982bd8b10289c5b9ed893214c1
SarumiofLagos <sarumioluwaseunideas@gmail.com>
2022-10-26 12:10:52 -0700

    task5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper - Changes all lowercase letters
+ * @str: the string to be changed
+ * Return: pointer to the changed string
+ */
+
+char *string_toupper(char *str)
+{
+	int index = 0;
+
+	while (str[index])
+	{
+		if (str[index] >= 'a' && str[index] <= 'z')
+			str[index] -= 32;
+
+		index++;
+	}
+
+	return (str);
+}
